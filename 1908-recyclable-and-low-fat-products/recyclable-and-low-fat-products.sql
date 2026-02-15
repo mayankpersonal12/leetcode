1218ee24889bb33981e2bce453be1785a77ee75f
Mayank Thakre <mayankpersonal1203@gmail.com>
2026-02-15 12:27:12 +0530

    Time: 507 ms (77.24%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-Select product_id
-from Products
-where low_fats='Y' and recyclable='Y'
+/* Write your PL/SQL query statement below */
+select product_id from Products where low_fats='Y' and recyclable='Y';
