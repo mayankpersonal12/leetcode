54f9b15d65f2b4cae190724712ba5de35ea6d26e
Mayank Thakre <mayankpersonal1203@gmail.com>
2026-02-15 12:51:07 +0530

    Time: 602 ms (26.74%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-select tweet_id from tweets
-where length(content)>15
-
+/* Write your PL/SQL query statement below */
+select tweet_id from Tweets where length(content)>15;
